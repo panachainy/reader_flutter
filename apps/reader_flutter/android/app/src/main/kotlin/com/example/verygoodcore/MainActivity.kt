1cacb87cf0a17535cdfdd9aaaa09d1a1a1d78d87
Panachai <panachai.ny@gmail.com>
2023-09-04 23:28:37 +0700

    init

---
+++ apps/reader_flutter/android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.reader_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps/reader_flutter/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:reader_flutter/app/app.dart';
+import 'package:reader_flutter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
